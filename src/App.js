5ffb43c60c2f7464c9ed28d4c2bfd9ae79375eb3
Rajiv <rajiv.i.ranjan@capgemini.com>
2020-05-23 21:47:43 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
@@ -9,9 +10,11 @@ class App extends Component {
 
   render() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Main/>
+      <Main />
     </div>
+  </BrowserRouter>
   );
 }
 }
