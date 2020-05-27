b320271fb2c9e78a057452b12309084e8148e2f7
Rajiv <rajiv.i.ranjan@capgemini.com>
2020-05-27 13:59:00 +0530

    Assigment week 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Home from './HomeComponent'
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -34,12 +35,22 @@ render() {
             />
         );
     }
+
+    const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+      };
+
     return (
       <div>
         <Header />
         <Switch>
             <Route path="/home" component={HomePage} />
-            <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes}  />} />
+            <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes}  />} />           
+            <Route exact path='/aboutus' component={()=> <About leaders={this.state.leaders} />} />} />
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />} />
             <Redirect to="/home" />
         </Switch>
